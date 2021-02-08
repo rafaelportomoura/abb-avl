ad02591883bb76cfd9b2cdb9787142037093797a
rafael.moura1 <rafael.moura1@estudante.ufla.br>
2021-02-08 18:19:59 -0300

    configuração dos arquivos

--- ABB/config.hpp
+++ ABB/config.hpp
@@ -3,8 +3,8 @@ int CONTADOR_NO_BUSCA = 0;
 int CONTADOR_NO_INSERCAO = 0;
 
 const int QUANTIDADE_DE_INSERCAO = 30;
-const string NOME_ARQUIVO_TESTE = "entrada.csv";
-const string NOME_ARQUIVO_ESCRITA = "abb.csv";
+const string NOME_ARQUIVO_TESTE = "../arquivos/entrada.csv";
+const string NOME_ARQUIVO_ESCRITA = "../arquivos/abb.csv";
 
 struct LINHA_CSV {
     Dado valor;
