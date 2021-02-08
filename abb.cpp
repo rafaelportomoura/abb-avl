29d8b1d8faacdd000ff70d90dd3588283d6baf53
rafael.moura1 <rafael.moura1@estudante.ufla.br>
2021-02-08 16:34:03 -0300

    busca

--- abb.cpp
+++ abb.cpp
@@ -4,19 +4,18 @@ typedef int Dado;
 
 using namespace std;
 
-class Noh
-{
+class Noh {
     friend class ArvoreABB;
-    private:
-        Noh* mEsquerdo;
-        Noh* mDireito;
-        Noh* mPai;
-        Dado valor;
-    public:
-        Noh(Dado d);
+private:
+    Noh* mEsquerdo;
+    Noh* mDireito;
+    Noh* mPai;
+    Dado valor;
+public:
+    Noh( Dado d );
 };
 
-Noh::Noh(Dado d){
+Noh::Noh( Dado d ) {
     //Construtor
     mEsquerdo = NULL;
     mDireito = NULL;
@@ -24,177 +23,198 @@ Noh::Noh(Dado d){
     valor = d;
 }
 
-class ArvoreABB
-{
-    private:
-        Noh* mRaiz;
-    public:
-        ArvoreABB();
-        ~ArvoreABB();
-        void insere(Dado d);
-        Noh* buscaAux(Noh* aux);
-        void transplanta(Noh* antigo, Noh* novo);
-        void remove(Dado d);
-        Noh* minimoAux(Noh* atual);
-        void D(){ destruct(mRaiz);}
-        void destruct(Noh* atual);
-        Dado Nivel(Dado chave);
+class ArvoreABB {
+private:
+    Noh* mRaiz;
+public:
+    ArvoreABB();
+    ~ArvoreABB();
+    void insere( Dado d );
+    Noh* buscaAux( Noh* aux );
+    void transplanta( Noh* antigo, Noh* novo );
+    void remove( Dado d );
+    Noh* minimoAux( Noh* atual );
+    void D() { destruct( mRaiz ); }
+    void destruct( Noh* atual );
+    Dado Nivel( Dado chave );
+    void Busca( Dado d );
 };
 
-ArvoreABB::ArvoreABB(){
+ArvoreABB::ArvoreABB() {
     //Contructor
     mRaiz = NULL;
 }
 
-ArvoreABB::~ArvoreABB(){
+ArvoreABB::~ArvoreABB() {
     //Destructor
     D();
 }
 
-void ArvoreABB::insere(Dado d){
+void ArvoreABB::insere( Dado d ) {
     //Insere interativamente
-    Noh* novo = new Noh(d);
-    if(mRaiz == NULL){
+    Noh* novo = new Noh( d );
+    if ( mRaiz == NULL ) {
         mRaiz = novo;
-    } else {
+    }
+    else {
         Noh* atual = mRaiz;
         Noh* ant = NULL;
-        
-        while(atual != NULL){
+
+        while ( atual != NULL ) {
             ant = atual;
-            if(atual -> valor > d){
-                atual = atual -> mEsquerdo;
-            } else {
-                atual = atual -> mDireito;
+            if ( atual->valor > d ) {
+                atual = atual->mEsquerdo;
+            }
+            else {
+                atual = atual->mDireito;
             }
         }
-        novo -> mPai = ant;
-        if(ant -> valor > novo -> valor){
-            ant -> mEsquerdo = novo;
-        } else {
-            ant -> mDireito = novo;
+        novo->mPai = ant;
+        if ( ant->valor > novo->valor ) {
+            ant->mEsquerdo = novo;
+        }
+        else {
+            ant->mDireito = novo;
         }
     }
 }
 
-Noh* ArvoreABB::buscaAux(Noh* aux){
+Noh* ArvoreABB::buscaAux( Noh* aux ) {
     //Faz a busca na estrutura e retorna
     Noh* atual = mRaiz;
-    while(atual != NULL){
-        if(atual -> valor == aux -> valor){
+    while ( atual != NULL ) {
+        if ( atual->valor == aux->valor ) {
             return atual;
-        } else if(atual -> valor > aux -> valor){
-            atual = atual -> mEsquerdo;
-        } else {
-            atual = atual -> mDireito;
+        }
+        else if ( atual->valor > aux->valor ) {
+            atual = atual->mEsquerdo;
+        }
+        else {
+            atual = atual->mDireito;
         }
     }
     return atual;
 }
 
-void ArvoreABB::transplanta(Noh* antigo, Noh* novo){
-    //Troca dois nos
-    if(mRaiz == antigo){
-        mRaiz = novo;
-    } else if(antigo == antigo -> mPai -> mEsquerdo){
-        antigo -> mPai -> mEsquerdo = novo;
-    } else {
-        antigo -> mPai -> mDireito = novo;
+void ArvoreABB::Busca( Dado d ) {
+    Noh* nohComValor = buscaAux( d );
+    if ( nohComValor == NULL ) {
+        cout << "NÃO ENCONTRADO";
     }
-    if(novo != NULL){
-        novo -> mPai = antigo -> mPai;
+    else {
+        cout << "ENCONTRADO";
     }
 }
 
-void ArvoreABB::remove(Dado d){
+void ArvoreABB::transplanta( Noh* antigo, Noh* novo ) {
+    //Troca dois nos
+    if ( mRaiz == antigo ) {
+        mRaiz = novo;
+    }
+    else if ( antigo == antigo->mPai->mEsquerdo ) {
+        antigo->mPai->mEsquerdo = novo;
+    }
+    else {
+        antigo->mPai->mDireito = novo;
+    }
+    if ( novo != NULL ) {
+        novo->mPai = antigo->mPai;
+    }
+}
+
+void ArvoreABB::remove( Dado d ) {
     //Remove um elemento (noh) da arvore
-    Noh* aux = new Noh(d);
-    Noh* remover = buscaAux(aux);
-    
-    if(remover == NULL){
+    Noh* aux = new Noh( d );
+    Noh* remover = buscaAux( aux );
+
+    if ( remover == NULL ) {
         cout << "ERRO" << endl;
-    } else {
-        if(remover -> mEsquerdo == NULL){
-            transplanta(remover, remover -> mDireito);
-        } else if(remover -> mDireito == NULL){
-            transplanta(remover, remover -> mEsquerdo);
-        } else {
-            Noh* sucessor = minimoAux(remover -> mDireito);
-            if(sucessor -> mPai != remover){
-                transplanta(sucessor, sucessor -> mDireito);
-                sucessor -> mDireito = remover -> mDireito;
-                sucessor -> mDireito -> mPai = sucessor;
+    }
+    else {
+        if ( remover->mEsquerdo == NULL ) {
+            transplanta( remover, remover->mDireito );
+        }
+        else if ( remover->mDireito == NULL ) {
+            transplanta( remover, remover->mEsquerdo );
+        }
+        else {
+            Noh* sucessor = minimoAux( remover->mDireito );
+            if ( sucessor->mPai != remover ) {
+                transplanta( sucessor, sucessor->mDireito );
+                sucessor->mDireito = remover->mDireito;
+                sucessor->mDireito->mPai = sucessor;
             }
-            transplanta(remover, sucessor);
-            sucessor -> mEsquerdo = remover -> mEsquerdo;
-            sucessor -> mEsquerdo -> mPai = sucessor;
+            transplanta( remover, sucessor );
+            sucessor->mEsquerdo = remover->mEsquerdo;
+            sucessor->mEsquerdo->mPai = sucessor;
         }
         delete remover;
     }
 }
 
-Noh* ArvoreABB::minimoAux(Noh* atual){
+Noh* ArvoreABB::minimoAux( Noh* atual ) {
     //Retorna o minimo da arvore
-    while(atual -> mEsquerdo != NULL){
-        atual = atual -> mEsquerdo;
+    while ( atual->mEsquerdo != NULL ) {
+        atual = atual->mEsquerdo;
     }
     return atual;
 }
 
-void ArvoreABB::destruct(Noh* atual){
-    
-    if(atual != NULL){
-        destruct(atual->mEsquerdo);
-        destruct(atual->mDireito);
+void ArvoreABB::destruct( Noh* atual ) {
+
+    if ( atual != NULL ) {
+        destruct( atual->mEsquerdo );
+        destruct( atual->mDireito );
         delete atual;
     }
 }
 
-Dado ArvoreABB::Nivel(Dado chave){
+Dado ArvoreABB::Nivel( Dado chave ) {
     Noh* atual = mRaiz;
     Dado cont = 0;
-    while(atual != NULL){
-        if(atual->valor == chave){
+    while ( atual != NULL ) {
+        if ( atual->valor == chave ) {
             return cont;
-        } else if(atual -> valor > chave){
-            atual = atual -> mEsquerdo;
-        } else {
-            atual = atual -> mDireito;
+        }
+        else if ( atual->valor > chave ) {
+            atual = atual->mEsquerdo;
+        }
+        else {
+            atual = atual->mDireito;
         }
         cont++;
     }
     return cont;
 }
 
-int main()
-{
+int main() {
     ArvoreABB minhaArvore;
     char escolha;
     cin >> escolha;
     int busca;
     Dado chave;
-	do{
-        switch(escolha){
-            case 'i':
-                cin >> chave;
-                minhaArvore.insere(chave);
-                break;
-            case 'r':
-                cin >> chave;
-                minhaArvore.remove(chave);
-                break;
-            case 'b':
-                cin >> busca;
-                //nao implementado (implementar na forma chave,valor [noh/busca])
-            case 'q':
-                break;
-            default:
-                cout << "Comando Invalido!";
-            }
-            cin >> escolha;
-    }while(escolha != 'q');
-    
-    
-	return 0;
+    do {
+        switch ( escolha ) {
+        case 'i':
+            cin >> chave;
+            minhaArvore.insere( chave );
+            break;
+        case 'r':
+            cin >> chave;
+            minhaArvore.remove( chave );
+            break;
+        case 'b':
+            cin >> busca;
+            //nao implementado (implementar na forma chave,valor [noh/busca])
+        case 'q':
+            break;
+        default:
+            cout << "Comando Invalido!";
+        }
+        cin >> escolha;
+    } while ( escolha != 'q' );
+
+
+    return 0;
 }
 
